cc191ac65085993945f4ba2e3a1b740265561f63
Johann <johannocsantos@gmail.com>
2021-02-19 18:24:23 -0300

    Implements the form to create a new event linked to the correct user/host

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,7 @@
 class Event < ApplicationRecord
+  belongs_to :host, class_name: 'User'
+
+  validates :name, presence: true
+  validates :place, presence: true
+  validates :date, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root to: 'events#index'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:show]
-  resources :events, only: [:show, :index]
+  resources :users, only: [:show] do
+    resources :events, only: [:new, :create]
+  end
+  resources :events, only: [:index, :show]
 end
