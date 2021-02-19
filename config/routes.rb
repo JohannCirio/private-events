577e1235541af44de17b78087856e55796911d52
Johann <johannocsantos@gmail.com>
2021-02-19 15:13:20 -0300

    modifies the devise user to have a name field

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root to: 'events#index'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show]
+  resources :events, only: [:show, :index]
 end
